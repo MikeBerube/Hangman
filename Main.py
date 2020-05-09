a53dd1dacc76e561ee62bc4b5775b39e3ebed8e3
MikeBerube <michaelberube98@gmail.com>
2020-05-09 05:31:11 -0400

    Fixed win conditions   Can now do words with multiple of the same letter   Fixes winning by typing one of the same correct letter

--- Main.py
+++ Main.py
@@ -3,33 +3,33 @@ word = 'hang'
 tries = 9
 guess = '0'
 guessed_letters = ''
-correct_guesses = 0
 while tries > 0:
+    missing_letters = 4
     for char in word:
         if char in guessed_letters:
             print(char, end=' ')
+            missing_letters -= 1
+            if missing_letters == 0:
+                print('\nYou Win!')
+                print('Would you like to play again. Type yes or no')
+                yes_or_no = input()
+                if yes_or_no == 'yes':
+                    tries = 9
+                    guessed_letters = ''
+                elif yes_or_no == 'no':
+                    print('Goodbye')
+                    quit()
         else:
             print('_', end=' ')
     print('\n\nYou have', tries, 'tries')
     guess = input('Guess a letter:\n')
     guessed_letters += guess
     tries -= 1
+
     if guess not in word:
         print('Wrong')
     else:
         print('Correct!')
-        correct_guesses += 1
-    if correct_guesses == len(word):
-        print('You Win!')
-        print('Would you like to play again. Type yes or no\n')
-        yes_or_no = input()
-        if yes_or_no == 'yes':
-            tries = 9
-            guessed_letters = ''
-            correct_guesses = 0
-        elif yes_or_no == 'no':
-            print('Goodbye')
-            quit()
     if tries == 0:
         print('You Lose')
         print('Would you like to play again. Type yes or no\n')
@@ -37,7 +37,6 @@ while tries > 0:
         if yes_or_no == 'yes':
             tries = 9
             guessed_letters = ''
-            correct_guesses = 0
         elif yes_or_no == 'no':
             print('Goodbye')
             quit()
